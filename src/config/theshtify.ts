6e7482083b935d354ebae4fbbb144d97f6497341
BEBE EPEE <epeeivan@gmail.com>
2025-09-07 01:16:32 +0100

    project installations

---
+++ src/config/theshtify.ts
@@ -0,0 +1,30 @@
+export const config =  {
+  x_align: 'right',
+  y_align: 'top',
+  duration: 5000,
+  font: {
+    size: 15,
+    weight: 900,
+    family: 'arlon',
+  },
+  custom_colors: {
+    success: {
+      bg: '#0C7059',
+      color: '#E0BC29',
+      border: {
+        type: 'solid',
+        color: 'gray',
+      },
+      progress: {
+        bg: '#E0BC29',
+      },
+    },
+    /** you can also add other types configs */
+  },
+  radius: 20,
+  bordered: true,
+  border_width: 1,
+  closer: true,
+  progress: true,
+  progress_height: 2,
+}
