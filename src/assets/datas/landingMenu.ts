4f209d6a4d4b87ef12daed19403e73d32dd53414
BEBE EPEE <epeeivan@gmail.com>
2025-09-09 00:04:03 +0100

    j'ai créé le composant de menu de la landing page

---
+++ src/assets/datas/landingMenu.ts
@@ -0,0 +1,18 @@
+export default [
+  {
+    text: 'labels.menu.home',
+    to: '',
+  },
+  {
+    text: 'labels.menu.programs',
+    to: '',
+  },
+  {
+    text: 'labels.menu.about',
+    to: '',
+  },
+  {
+    text: 'labels.menu.news',
+    to: '',
+  },
+]
