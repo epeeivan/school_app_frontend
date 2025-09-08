bf03ad2044c6f3315fe965b2cc33d0977d6d1185
BEBE EPEE <epeeivan@gmail.com>
2025-09-08 22:58:42 +0100

    i created the landing menu item component

---
+++ src/components/headers/landing/MenuItem.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+const props = defineProps({
+  text: {
+    type: String,
+    default: '',
+  },
+  to: {
+    type: String,
+    default: '',
+  },
+})
+const router = useRouter()
+const route = useRoute()
+const isActive = computed(() => route.name === props.to)
+
+function trigg() {
+  if (props.to) {
+    router.push({ name: props.to })
+  }
+}
+</script>
+<template>
+  <a
+    href=""
+    class="block rounded-[8px] px-5 py-2.5"
+    :class="isActive ? 'bg-secondary-100' : ''"
+    @click="trigg"
+  >
+    <span class="block">{{ text }}</span>
+  </a>
+</template>
