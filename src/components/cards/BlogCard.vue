c07061bd91e40d6c5c3d8244c4529fc0147f7e85
BEBE EPEE <epeeivan@gmail.com>
2025-09-16 11:37:07 +0100

    i created the blog card component

---
+++ src/components/cards/BlogCard.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+  src: {
+    type: String,
+    default: '',
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+<template>
+  <div class="space-y-5">
+    <!-- image -->
+    <div
+      class="block min-h-[300px] max-h-[300px] w-full rounded-[20px]"
+      :style="`background-image:src(${src})`"
+    ></div>
+    <div class="flex flex-col space-y-2">
+      <span class="block">{{ title }}</span>
+      <span class="block text-xs">{{ description }}</span>
+    </div>
+  </div>
+</template>
---
+++ src/components/cards/FormationCard.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+  actionText: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+<template>
+  <div class="p-5 rounded-[20px] border border-neutral-300 space-y-5">
+    <!-- title & description -->
+    <div class="flex flex-col space-y-5">
+      <h1 class="font-bold lg:text-lg text-sm">
+        {{ title }}
+      </h1>
+      <p class="">{{ description }}</p>
+    </div>
+    <!-- action -->
+    <div class="flex pt-5 border-neutral-300">
+      <button class="block font-bold text-center mx-auto">{{ actionText }}</button>
+    </div>
+  </div>
+</template>
